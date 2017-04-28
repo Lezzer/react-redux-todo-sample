d6d3f9cda9bccd600aef779b96655d0a191386fd
Liam Reilly <liam.reilly@zuto.com>
2017-04-28 16:22:13 +0100

    Pushing so I can work from home

--- react-redux-todo/src/App.js
+++ react-redux-todo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ADD_TODO, REMOVE_TODO, addTodo } from './actions/actions'
 
 class App extends Component {
   render() {
@@ -8,10 +9,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Todo App</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          What do you want to do today?
         </p>
       </div>
     );
